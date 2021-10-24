c6f3663b41b9879742889d54f5c739615388abb7
Benjamin LEVALTIER <benji1705@hotmail.fr>
2021-10-24 18:17:35 +0200

    MERDE

--- src/board.js
+++ src/board.js
@@ -6,33 +6,31 @@ export default class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      styleMain :{
-        backgroundColor: "",
-      },
+      backgroundColor: "#ff0000",
     }
     this.changeStyle = this.changeStyle.bind(this);
+  }
+
+
+  changeStyle() {
+    var chaine = "0123456789abcdef";
+    var styleBckg = "#";
+    for (let i = 0; i <= 6; i++) {
+      styleBckg += chaine.charAt(Math.random() * (16));
     }
+    console.log(styleBckg);
+    this.setState({ backgroundColor: styleBckg });
+  }
+
 
   render() {
-    let profil = this.props.profil;
+    // let profil = this.props.profil;
 
     return (
-      <main className="board" style={this.state.styleMain}>
+      <main className="board" style={{background : this.state.backgroundColor}}>
         <button onClick={this.changeStyle}>Change Style </button>
         <Picture />
       </main>
     );
   }
-
-  changeStyle() {
-    console.log('changeStyle');
-    var chaine = "0123456789abcdef";
-    var styleBckg = "#";
-    for (let i = 0; i <= 6; i++) {
-      styleBckg += chaine.charAt(Math.random() * (16));
-      console.log(styleBckg);
-    }
-    this.setState({styleMain:{backgroundColor: styleBckg},},);
-  }
-
 }
