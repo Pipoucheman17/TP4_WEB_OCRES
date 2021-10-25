e2f1146fd502615914b53796aa5c685d05f99e3d
Benjamin Levaltier <benji1705@hotmail.fr>
2021-10-25 09:00:16 +0200

    Commit change style qui marche

--- src/board.js
+++ src/board.js
@@ -6,20 +6,19 @@ export default class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      backgroundColor: "#ff0000",
+      backgroundColor: "#ff1234",
     }
-    this.changeStyle = this.changeStyle.bind(this);
   }
 
 
   changeStyle() {
-    var chaine = "0123456789abcdef";
+    var chaine = "0123456789ABCDEF";
     var styleBckg = "#";
-    for (let i = 0; i <= 6; i++) {
-      styleBckg += chaine.charAt(Math.random() * (16));
+    for (let i = 0; i < 6; i++) {
+      styleBckg += chaine.charAt(Math.random() * (15-1)+1);
     }
-    console.log(styleBckg);
     this.setState({ backgroundColor: styleBckg });
+    
   }
 
 
@@ -27,10 +26,10 @@ export default class Board extends React.Component {
     // let profil = this.props.profil;
 
     return (
-      <main className="board" style={{background : this.state.backgroundColor}}>
-        <button onClick={this.changeStyle}>Change Style </button>
+      <div className="board" style={{background : this.state.backgroundColor}}>
+        <button onClick={() => this.changeStyle()}>Change Style </button>
         <Picture />
-      </main>
+      </div>
     );
   }
 }
