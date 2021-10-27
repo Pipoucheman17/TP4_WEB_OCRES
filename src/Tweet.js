5b0d4927703c5954cf22a3533d6c0e2b0d969fe9
Benjamin LEVALTIER <benji1705@hotmail.fr>
2021-10-27 17:34:34 +0200

    Partie principale fini, reste plus que le bonus a like

---
+++ src/Tweet.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+export default class Tweet extends React.Component{
+
+    render(){
+        return(
+            <div className="container-fluid tweet">
+                <div>
+                    
+                </div>
+
+            </div>
+        );
+    }
+}
