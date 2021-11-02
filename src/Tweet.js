5ba530682e30a33da41dacd6651bd70c6084397c
Benjamin Levaltier <benji1705@hotmail.fr>
2021-11-02 19:11:50 +0100

    Bonus pas fini (reste a faire les profiles en const pour ajouter un compteur de Like par profile)

--- src/Tweet.js
+++ src/Tweet.js
@@ -1,16 +1,34 @@
 import React from "react";
+import Omnes from "./Omnes.PNG";
+
+export default class Tweet extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            LikebuttonStyle: "container-fluid",
+            likes :this.props.likes,
+        };
+    }
 
 
-export default class Tweet extends React.Component{
-
-    render(){
-        return(
+    render() {
+        return (
             <div className="container-fluid tweet">
                 <div>
-                    
+                    <img src={Omnes} width="80%" alt="Omnes"/>
+                    <button className={this.state.buttonClassName} style={{margin:"10px"}} onClick={()=> this.addLikes()} >Likes {this.state.likes}</button>
                 </div>
 
             </div>
         );
+    }
+
+    addLikes() {
+        this.setState({likes: this.state.likes +1})
+    }
+
+    handleChange(event) {
+        this.props.onChangeLikes(event.target.value);
     }
 }
--- src/page.js
+++ src/page.js
@@ -14,6 +14,7 @@ export default class Page extends React.Component {
                 prenom: "Michel",
                 age: "23/04/1979",
                 className2: "board",
+                likes: 0,
                 backgroundColor: "#ffffff",
             }
         };
@@ -23,18 +24,25 @@ export default class Page extends React.Component {
 
     render() {
         return (
-            <div className="container-fluid" style={{padding: "10%", paddingTop: "0%"}}>
+            <div className="container-fluid" style={{ padding: "10%", paddingTop: "0%" }}>
                 <div className="row justify-content-around">
                     <button className={this.state.Buttonstyle} onClick={() => this.profilMichel()}>Michel</button>
                     <button className={this.state.Buttonstyle} onClick={() => this.profilClaude()}>Claude</button>
                     <button className={this.state.Buttonstyle} onClick={() => this.profilAnne()}>Anne</button>
                 </div>
                 <Board profil={this.state.profil} className={this.state.profil.className2} />
-                <Tweet/>
+                <Tweet likes={this.state.profil.likes} />
             </div>
         );
     }
 
+    handleChangeLikes(likes) {
+        this.setState({
+            profil: {
+                likes: likes
+            }
+        });
+    }
 
     profilMichel() {
         this.setState({
@@ -42,7 +50,7 @@ export default class Page extends React.Component {
                 nom: "Durant",
                 prenom: "Michel",
                 age: "23/04/1979",
-                className2: "board"
+                className2: "board",
             }
         });
     }
@@ -53,18 +61,20 @@ export default class Page extends React.Component {
                 nom: "Pont",
                 prenom: "Claude",
                 age: "13/06/1975",
-                className2: "board"
+                className2: "board",
+
             }
         });
     }
 
-    profilAnne(){
+    profilAnne() {
         this.setState({
             profil: {
-                nom:"Lamarre",
-                prenom:"Anne",
-                age:"12/12/1976",
-                className2:"board"
+                nom: "Lamarre",
+                prenom: "Anne",
+                age: "12/12/1976",
+                className2: "board",
+
             }
         })
     }
